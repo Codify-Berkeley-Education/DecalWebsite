26cfffe8e216593d4d93cbb51423be167dfb323e
aidansunbury <aidansunbury@gmail.com>
2023-09-04 12:41:09 -0700

    added some resources

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -12,9 +12,17 @@ export const schedule: lecture[] = [
   },
   {
     title: "JavaScript",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1ScgDd2E9kQaIz2Lnu08eNXCUIFBdbpOwyMWqxfVD3NA/edit?usp=sharing",
+    lectureCodeLink:
+      "https://codesandbox.io/p/sandbox/lecture-2-code-javascript-gd5kjy?file=/index.js:3,1",
   },
   {
     title: "TypeScript",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1XR1-FW7A4egR6jCSyddYZLoIvqiFxl_CVhUmhvRUnkM/edit?usp=sharing",
+    lectureCodeLink:
+      "https://codesandbox.io/p/sandbox/lecture-3-typescript-hyshrm",
   },
   {
     title: "API Basics",
